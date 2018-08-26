89dcb2b5a121de51dd6a263a9c4f1059d1ab9024
candra95 <42297891+candra95@users.noreply.github.com>
2018-08-26 00:49:25 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 ##############################################################################################################
-$config['urls'] = 'hUDJSNChdiejxxxxxxcrypto2'; //from urls
-$config['sessid'] = 'vcvbuo9sv137rg4l8gaxxxxxx'; //from http cookie PHPSESSID headers
+$config['urls'] = 'hUDJSNChdiej73dosN8S7KcyJneEdcrypto2'; //from urls
+$config['sessid'] = 'lgjv47jnudu90nud3a5g7v75q0'; //from http cookie PHPSESSID headers
 $config['worker'] = '50'; //choose wisely, be careful, too much worker causing banned account
 ##############################################################################################################
